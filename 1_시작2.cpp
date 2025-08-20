358fbfeb64cd9f710d726a8bc36dc57c903aa6a3
Chansik Yun <chansigi@gmail.com>
2025-08-20 10:52:26 +0900

    .

--- 1_시작2.cpp
+++ 1_시작2.cpp
@@ -33,7 +33,61 @@ int main()
 #include <iostream> // 표준 입출력 라이브러리 헤더 파일
                     // C++ 표준 라이브러리의 헤더 파일은 .h 라는 확장자가 없습니다.
 
+// std::cout: 표준 출력
+
+#if 0
 int main()
 {
     std::cout << "Hello, C++" << std::endl;
+
+    int n = 42;
+    std::cout << n << std::endl;
+}
+#endif
+
+#if 0
+int main()
+{
+    // C - printf/scanf
+    double n1;
+    scanf("%lf", &n1);
+    printf("%lf\n", n1);
+    // 문제점: 변수의 타입이 변경되면, 서식도 변경되어야 합니다.
+
+    // C++ - std::cout/std::cin
+    // -> 변수의 타입을 기반으로 입출력이 수행됩니다.
+    //    변수의 타입을 변경하여도, 기존 std::cout / std::cin에 대한 코드를
+    //    변경할 필요가 없습니다.
+    double n2;
+    std::cin >> n2;
+    std::cout << n2 << std::endl;
+}
+#endif
+
+#if 0
+int main()
+{
+    // C - printf/scanf
+    double n1;
+    scanf("%lf", &n1);
+    printf("n=%lf\n", n1);
+
+    // C++ - std::cout/std::cin
+    double n2;
+    std::cin >> n2;
+    std::cout << "n=" << n2 << std::endl;
+    // 출력의 결과를 조합하는데 어려움이 있습니다.
+}
+#endif
+
+// C++23 - 새로운 출력 함수가 도입되었습니다.
+#include <print>
+// g++ 1_시작2.cpp -std=c++23
+
+int main()
+{
+    double n = 3.14;
+
+    std::print("n={}", n);
+    std::println("n={}", n);
 }
